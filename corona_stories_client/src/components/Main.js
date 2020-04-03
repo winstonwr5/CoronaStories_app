5cf70eb6daec11b673b44bcd4e9672708d803419
William Winston <winston.william@gmail.com>
2020-04-03 13:30:21 -0400

    App.js, Main.js, Post.js preliminaries completed

--- corona_stories_client/src/components/Main.js
+++ corona_stories_client/src/components/Main.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Post from './Post.js'
 
 class Main extends React.Component {
     state = {
@@ -22,7 +23,61 @@ class Main extends React.Component {
             let data = await response.json()
             this.props.handleView('home')
             this.setState(prevState => {
-                return { stories: [...prevState.stories]}
+                return { stories: [...prevState.stories, data]}
             })
     }
+
+    handleUpdate = async updateData => {
+        let response = await fetch(`http://localhost:3000/stories/${updateData.id}`, {
+        body: JSON.stringify(updateData),
+        method: 'PUT',
+        headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        }
+    })
+    let data = await response.json()
+    this.props.handleView('home')
+    this.fetchStories()
+    }
+
+    handleDelete = async id => {
+        let response = await fetch(`http://localhost:3000/posts/${id}`, {
+            method: 'DELETE',
+            headers: {
+                Accept: 'application/json, text/plain, */*', 'Content-Type': 'application/json'
+            }
+        })
+        this.setState(prevState => {
+            const stories = prevState.stories.filter(story => story.id !== id)
+            return { stories }
+        })
+    }
+
+    componentDidMount() {
+        this.fetchStories()
+    }
+
+    render () {
+        return (
+            <main>
+                <h1>{this.props.view.pageTitle}</h1>
+                {this.props.view.page === 'home' ?
+                this.state.stories.map(story => (
+                    <Post key={story.id} story={story}
+                    handleView={this.props.handleView}
+                    handleDelete={this.handleDelete}
+                     />
+                ))
+            :
+                <Form handleCreate={this.handleCreate}
+                formInputs={this.props.formInputs}
+                view={this.props.view}
+                handleUpdate={this.handleUpdate}/>
+                }
+            </main>
+        )
+    }
 }
+
+export default Main
