1aeebd9a473e8c80273264d58e209eb870ea40c3
Madhav Pokhrel <saraswatiacharya05@gmail.com>
2020-04-03 13:38:24 -0400

    added files

---
+++ corona_stories_client/src/components/Form.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+class Form extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      body: '',
+      id: null
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.id] : e.target.value})
+  }
+
+  // handles submit
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.handleCreate(this.state)
+  }
+
+  componentDidMount() {
+    this.setState({
+      name: this.props.formInputs.name,
+      body: this.props.formInputs.body,
+      id: this.props.formInputs.id
+    })
+  }
+
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          name
+          <input type="text" placeholder="your name" id="name" value={this.state.name} onChange={this.handleChange}/>
+        </label>
+        <label id="story-form">
+          story
+          <textarea placeholder="Write you coronavirus story here" id="body" value={this.state.body} onChange={this.handleChange}></textarea>
+        </label>
+        <input type="submit" value="share"/>
+      </form>
+    )
+  }
+}
+
+export default Form
